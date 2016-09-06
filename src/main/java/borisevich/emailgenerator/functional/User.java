46324736abcb39ac4f1032b4a6669a895e35fb69
Leonid Borisevich <leonidborisevich00@gmail.com>
2016-09-06 19:04:14 +0300

    Added missing User constructor with 2 params

--- src/main/java/borisevich/emailgenerator/functional/User.java
+++ src/main/java/borisevich/emailgenerator/functional/User.java
@@ -8,6 +8,11 @@ public class User {
     private String username;
     private String password;
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public User(int user_id, String username, String password){
         this.user_id = user_id;
         this.username = username;
